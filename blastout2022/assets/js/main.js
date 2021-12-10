8a8dfc2b32c2d4bab96c4bc3ab7dc62252b4b906
pramanarendra <74599808+pramanarendra@users.noreply.github.com>
2021-12-10 16:12:02 +0700

    Delete blastout2022 directory

--- blastout2022/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-//toggle navbar
-var hamburger = document.querySelector(".hamburger");
-var menu = document.querySelector(".menu");
-
-//toggle navbar function
-hamburger.addEventListener("click", function () {
-    hamburger.classList.toggle("is-active");
-    menu.classList.toggle("slide")
-});
