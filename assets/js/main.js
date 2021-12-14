c19902fa917bd0929875a14059d6f587c4ca7521
pramanarendra <74599808+pramanarendra@users.noreply.github.com>
2021-12-14 11:24:31 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,13 @@ hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("is-active");
     menu.classList.toggle("slide")
 });
+
+$(document).ready(function () {
+    $('#autoWidth').lightSlider({
+        autoWidth: true,
+        loop: true,
+        onSliderLoad: function () {
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    });
+});
