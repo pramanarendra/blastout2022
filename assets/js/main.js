704bee77a0c7daa5cdc34202bf4b07fa0c4ac743
pramanarendra <74599808+pramanarendra@users.noreply.github.com>
2021-12-25 22:50:25 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
 //toggle navbar
 var hamburger = document.querySelector(".hamburger");
 var menu = document.querySelector(".menu");
+//toggle navbar
+var guide = document.querySelector(".btn-main");
+var blurbg = document.getElementById("blurbg");
+var popup = document.getElementById("pop-up");
+var close = document.querySelector(".close");
+//parallax
+const parallax = document.getElementById("landing-page");
+const parallax2 = document.getElementById("about");
+const parallax3 = document.getElementById("timeline");
+const parallax4 = document.getElementById("testimonial");
+
 
 //toggle navbar function
 hamburger.addEventListener("click", function () {
@@ -17,3 +28,26 @@ $(document).ready(function () {
         }
     });
 });
+
+//toggle pop up
+guide.addEventListener("click", function () {
+    guide.classList.toggle("active");
+    blurbg.classList.toggle("active");
+    popup.classList.toggle("active");
+});
+
+close.addEventListener("click", function () {
+    guide.classList.remove("active");
+    blurbg.classList.remove("active");
+    popup.classList.remove("active");
+})
+
+
+//parallax background
+window.addEventListener("scroll", function () {
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset * 0.7 + "px";
+    parallax2.style.backgroundPositionY = offset * 0.08 + "px";
+    parallax3.style.backgroundPositionY = offset * 0.7 + "px";
+    parallax4.style.backgroundPositionY = offset * 0.1 + "px";
+})
